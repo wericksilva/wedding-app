a9231cb15111ef44479923d08d8467666e5bf5a3
C1281964 Werick Silva Rodrigues Cruz <wericksilva-@hotmail.com>
2024-06-28 17:24:49 -0300

    historia

--- src/app/page.js
+++ src/app/page.js
@@ -57,7 +57,11 @@ export default function Home() {
                             </div>
                         </div>
                         <div class="card-body">
-                            Nossa História
+                       
+                                <svg xmlns="http://www.w3.org/2000/svg" color="green" width="42" height="60" fill="currentColor" class="bi bi-film" viewBox="0 0 16 16">
+                                    <path d="M0 1a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1zm4 0v6h8V1zm8 8H4v6h8zM1 1v2h2V1zm2 3H1v2h2zM1 7v2h2V7zm2 3H1v2h2zm-2 3v2h2v-2zM15 1h-2v2h2zm-2 3v2h2V4zm2 3h-2v2h2zm-2 3v2h2v-2zm2 3h-2v2h2z" />
+                                </svg>
+                            
                         </div>
 
                     </div>
