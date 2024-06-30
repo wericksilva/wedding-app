1cb192bfbf4b6a28889a90154ce90f45f62d61bd
C1281964 Werick Silva Rodrigues Cruz <wericksilva-@hotmail.com>
2024-06-30 12:23:15 -0300

    textos cards

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
     const [timeLeft, setTimeLeft] = useState('');
 
     useEffect(() => {
-        const countDownDate = new Date("Feb 8, 2025 00:00:00").getTime();
+        const countDownDate = new Date("Feb 8, 2025 11:00:00").getTime();
 
         const updateCountdown = () => {
             const now = new Date().getTime();
@@ -41,13 +41,16 @@ export default function Home() {
             </div>
 
             <div className="container">
-                <div className="text-center mt-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16">
-                        <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z" />
-                        <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0" />
+                <div className="text-center mt-2 d-flex align-items-center justify-content-center">
+
+                    <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="currentColor" class="bi bi-alarm" viewBox="0 0 16 16">
+                        <path d="M8.5 5.5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9z" />
+                        <path d="M6.5 0a.5.5 0 0 0 0 1H7v1.07a7.001 7.001 0 0 0-3.273 12.474l-.602.602a.5.5 0 0 0 .707.708l.746-.746A6.97 6.97 0 0 0 8 16a6.97 6.97 0 0 0 3.422-.892l.746.746a.5.5 0 0 0 .707-.708l-.601-.602A7.001 7.001 0 0 0 9 2.07V1h.5a.5.5 0 0 0 0-1zm1.038 3.018a6 6 0 0 1 .924 0 6 6 0 1 1-.924 0M0 3.5c0 .753.333 1.429.86 1.887A8.04 8.04 0 0 1 4.387 1.86 2.5 2.5 0 0 0 0 3.5M13.5 1c-.753 0-1.429.333-1.887.86a8.04 8.04 0 0 1 3.527 3.527A2.5 2.5 0 0 0 13.5 1" />
                     </svg>
-                    <h5>{timeLeft}</h5>
+                    <p className="ms-2 mb-0 align-middle">{timeLeft}</p>
                 </div>
+
+
             </div>
 
             <div className="container text-center">
@@ -58,7 +61,7 @@ export default function Home() {
                         </div>
                     </div>
                     <div className="card-body">
-                        {/* Other content */}
+                        <p>Nos conhecemos em uma tarde ensolarada e, desde então, nossas vidas se entrelaçaram em uma linda história de amor. Venha descobrir cada capítulo dessa jornada incrível que nos trouxe até aqui.</p>
                     </div>
                 </div>
 
@@ -71,7 +74,7 @@ export default function Home() {
                         </a>
                     </div>
                     <div className="card-body">
-                        {/* Other content */}
+                        <p>Junte-se a nós para celebrar o amor e a união em nossa cerimônia especial. Sua presença tornará este momento ainda mais inesquecível.</p>
                     </div>
                 </div>
 
@@ -84,7 +87,7 @@ export default function Home() {
                         </a>
                     </div>
                     <div className="card-body">
-                        {/* Other content */}
+                        <p>Venha comemorar conosco em nossa recepção! Será uma tarde de alegria, dança e muitas lembranças especiais.</p>
                     </div>
                 </div>
 
@@ -97,7 +100,7 @@ export default function Home() {
                         </a>
                     </div>
                     <div className="card-body">
-                        {/* Other content */}
+                        <p>Confirme sua presença e nos ajude a tornar este dia ainda mais especial. Estamos ansiosos para celebrar com você!</p>
                     </div>
                 </div>
 
@@ -108,7 +111,7 @@ export default function Home() {
                         </div>
                     </div>
                     <div className="card-body">
-                        {/* Other content */}
+                        <p>Sua presença é o nosso maior presente, mas se desejar nos presentear, veja nossas sugestões com carinho. Agradecemos de coração!</p>
                     </div>
                 </div>
             </div>
