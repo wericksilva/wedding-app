006f195b301894e9efeda3b26bef3e21f8e2adf6
C1281964 Werick Silva Rodrigues Cruz <wericksilva-@hotmail.com>
2024-07-24 13:00:09 -0300

    manual padrinhos

--- src/app/padrinhos/page.js
+++ src/app/padrinhos/page.js
@@ -48,50 +48,48 @@ export default function Home() {
         </div>
       </div>
 
+      <div class="container" style={{ marginTop: "10px" }} >
+        <div class="row">
 
+          <div class="col-12 mb-2">
+            <div class="card">
+              <div className="text-center">
+                <img src="./imagens/manual1.svg" width="400px" height="220px" className="me-12" alt="manual1" />
+              </div>
+              
+            </div>
+          </div>
 
-      <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
-    <div class="carousel-indicators">
-      <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-      <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    </div>
-    <div class="carousel-inner">
-      <div class="carousel-item active" data-bs-interval="10000">
-        <img src="./imagens/manualPadrinhos.svg" class="d-block w-100" alt="manualPadrinhos"/>
-        <div class="carousel-caption d-none d-md-block">
-          <h5>First slide label</h5>
-          <p>Some representative placeholder content for the first slide.</p>
-        </div>
-      </div>
-      <div class="carousel-item" data-bs-interval="2000">
-        <img src="./imagens/manualPadrinhos.svg" class="d-block w-100" alt="manualPadrinhos"/>
-        <div class="carousel-caption d-none d-md-block">
-          <h5>Second slide label</h5>
-          <p>Some representative placeholder content for the second slide.</p>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <img src="./imagens/manualPadrinhos.svg" class="d-block w-100" alt="manualPadrinhos"/>
-        <div class="carousel-caption d-none d-md-block">
-          <h5>Third slide label</h5>
-          <p>Some representative placeholder content for the third slide.</p>
-        </div>
-      </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
-  </div>
+          </div>
 
-  
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/5.0.2/js/bootstrap.bundle.min.js"></script>
+          <div class="row">
 
+          <div class="col-12 mb-2">
+            <div class="card">
+              <div className="text-center">
+                <img src="./imagens/manual2.svg" width="400px" height="220px" className="me-12" alt="manual2" />
+              </div>
+              
+            </div>
+          </div>
+
+          </div>
+
+          <div class="row">
+
+          <div class="col-12 mb-2">
+            <div class="card">
+              <div className="text-center">
+                <img src="./imagens/manual3.svg" width="400px" height="220px" className="me-12" alt="manual3" />
+              </div>
+              
+            </div>
+          </div>
+
+          </div>
+          </div>
+
+    
 
     </>
   );
