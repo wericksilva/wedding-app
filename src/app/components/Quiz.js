3104ddd963c19c37da7161ab3b93c12ea8ebec74
C1281964 Werick Silva Rodrigues Cruz <wericksilva-@hotmail.com>
2024-12-02 16:50:12 -0300

    quiz 100

--- src/app/components/Quiz.js
+++ src/app/components/Quiz.js
@@ -145,19 +145,18 @@ const Quiz = () => {
     const renderStars = (numStars) => {
         const totalStars = 5;
         let stars = [];
-
+    
         for (let i = 0; i < totalStars; i++) {
             stars.push(
                 <svg
                     key={i}
                     xmlns="http://www.w3.org/2000/svg"
-                    width="1em"   // Ajuste o tamanho da estrela
-                    height="1em"  // Ajuste o tamanho da estrela
-                    fill={i < numStars ? "yellow" : "gray"}  // Preenche a estrela de amarelo ou cinza
-
-
+                    width="1.3em"   // Ajuste o tamanho da estrela
+                    height="1.3em"  // Ajuste o tamanho da estrela
+                    fill={i < numStars ? "gold" : "gray"}  // Preenche a estrela de ouro ou cinza
                     className="bi bi-star"
                     viewBox="0 0 16 16"
+                  
                 >
                     <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z" />
                 </svg>
@@ -165,6 +164,7 @@ const Quiz = () => {
         }
         return stars;
     };
+    
 
 
 
