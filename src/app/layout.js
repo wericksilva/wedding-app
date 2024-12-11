15531efb2c312336d314bce5467013195e904a0e
C1281964 Werick Silva Rodrigues Cruz <wericksilva-@hotmail.com>
2024-12-11 20:31:30 -0300

    ultimo teste icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,16 +5,15 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Casamento Samanta & Werick",
-  description: "Casamento Samanta & Werickapp",
+  description: "Casamento Samanta & Werick",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width:device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* Adicionando o favicon */}
-        {/* Ícones para favicons e atalhos */}
         <link rel="apple-touch-icon" sizes="120x120" href="/img/favicon/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png" />
@@ -23,7 +22,6 @@ export default function RootLayout({ children }) {
 
         {/* Manifest para PWA */}
         <link rel="manifest" href="/img/favicon/site.webmanifest" />
-
       </head>
       <body>{children}</body>
     </html>
