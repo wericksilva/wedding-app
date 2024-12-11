655e0dba925eae53cfdb25d7082c66e9b2608bf9
C1281964 Werick Silva Rodrigues Cruz <wericksilva-@hotmail.com>
2024-12-11 20:12:06 -0300

    favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,15 +14,16 @@ export default function RootLayout({ children }) {
       <head>
         <meta name="viewport" content="width:device-width, initial-scale=1" />
         {/* Adicionando o favicon */}
+        {/* Ícones para favicons e atalhos */}
         <link rel="apple-touch-icon" sizes="120x120" href="/img/favicon/apple-touch-icon.png" />
-  <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png" />
-  <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png" />
-  <link rel="icon" type="image/png" sizes="192x192" href="/img/favicon/android-chrome-192x192.png" />
-  <link rel="icon" type="image/png" sizes="512x512" href="/img/favicon/android-chrome-512x512.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/img/favicon/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/img/favicon/android-chrome-512x512.png" />
 
-  {/* Manifest para PWA */}
-  <link rel="manifest" href="/img/favicon/site.webmanifest" />
- 
+        {/* Manifest para PWA */}
+        <link rel="manifest" href="/img/favicon/site.webmanifest" />
+
       </head>
       <body>{children}</body>
     </html>
