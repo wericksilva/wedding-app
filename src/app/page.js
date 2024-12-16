eadb2a66023be71ea9060ffd60bd880cda7de4af
C1281964 Werick Silva Rodrigues Cruz <wericksilva-@hotmail.com>
2024-12-16 11:08:00 -0300

    teste item  de menu

--- src/app/page.js
+++ src/app/page.js
@@ -169,102 +169,157 @@ export default function Home() {
 
 
             <div className="container text-center">
-                <div className="mt-2">
-                    <div className="container">
-                        <a href="/wedding-app/historia">
-                            <div className="text-center">
-                                <img src="./imagens/historia.svg" width="352px" height="205px" className="me-12" alt="Historia" />
-                            </div>
-                        </a>
-                    </div>
-                    <div className="card-body mt-4">
+                
 
-                        <p>Nos conhecemos ainda crianças, em 2004, e acompanhamos um ao outro ao longo de todos esses anos. Vimos a infância se transformar em adolescência, a adolescência em juventude, e agora, com a maturidade dos adultos, olhamos para trás com gratidão e carinho. Nossa amizade e cumplicidade cresceram e se fortaleceram com o tempo, e temos a certeza de que estamos prontos para iniciar um casamento centrado em Cristo.</p>
-                    </div>
-                </div>
+            <div className="mt-4">
+    <div className="container">
+        <div className="row justify-content-center">
+            <div className="col-12 col-md-8 col-lg-6 text-center">
+                <a href="/wedding-app/historia" className="d-inline-block link-clicavel">
+                    <img 
+                        src="./imagens/historia.svg" 
+                        className="img-fluid rounded-3 shadow-lg hover-scale" 
+                        alt="Nossa história"
+                    />
+                </a>
+            </div>
+        </div>
+    </div>
+    <div className="card-body mt-4">
+        <p className="text-justify fs-5">
+            Nos conhecemos ainda crianças, em 2004, e acompanhamos um ao outro ao longo de todos esses anos. 
+            Vimos a infância se transformar em adolescência, a adolescência em juventude, e agora, com a maturidade dos adultos, 
+            olhamos para trás com gratidão e carinho. Nossa amizade e cumplicidade cresceram e se fortaleceram com o tempo, 
+            e temos a certeza de que estamos prontos para iniciar um casamento centrado em Cristo.
+        </p>
+    </div>
+</div>
 
-                <div className="mt-4">
-                    <div className="container">
-                        <a className="nav-link" target="_blank" href="https://maps.app.goo.gl/q9tLoYmP62dTkNzM9">
-                            <div className="text-center">
-                                <img src="./imagens/cerimonia.svg" width="352px" height="205px" className="me-12" alt="Cerimonia" />
-                            </div>
-                        </a>
-                    </div>
-                    <div className="card-body">
-                        <p>É com imensa alegria que convidamos você para celebrar conosco o nosso casamento, que acontecerá no dia 08 de fevereiro de 2025, às 11h30, na Catedral Metropolitana de Brasília, Nossa Senhora Aparecida. Este será um momento muito especial para nós, e sua presença será uma grande honra.</p>
-                    </div>
-                </div>
+<div className="mt-4">
+    <div className="container">
+        <div className="row justify-content-center">
+            <div className="col-12 col-md-8 col-lg-6 text-center">
+                <a href="https://maps.app.goo.gl/q9tLoYmP62dTkNzM9" target="_blank" className="d-inline-block link-clicavel">
+                    <img 
+                        src="./imagens/cerimonia.svg" 
+                        className="img-fluid rounded-3 shadow-lg hover-scale" 
+                        alt="Cerimônia"
+                    />
+                </a>
+            </div>
+        </div>
+    </div>
+    <div className="card-body mt-4">
+        <p className="text-justify fs-5">
+            É com imensa alegria que convidamos você para celebrar conosco o nosso casamento, que acontecerá no dia 08 de fevereiro de 2025, às 11h30, na Catedral Metropolitana de Brasília, Nossa Senhora Aparecida. Este será um momento muito especial para nós, e sua presença será uma grande honra.
+        </p>
+    </div>
+</div>
 
-                <div className="mt-4">
-                    <div className="container">
+<div className="mt-4">
+    <div className="container">
+        <div className="row justify-content-center">
+            <div className="col-12 col-md-8 col-lg-6 text-center">
+                <a href="https://maps.app.goo.gl/ww61LAaK9ZgrtEeW8" target="_blank" className="d-inline-block link-clicavel">
+                    <img 
+                        src="./imagens/recepcao.svg" 
+                        className="img-fluid rounded-3 shadow-lg hover-scale" 
+                        alt="Recepção"
+                    />
+                </a>
+            </div>
+        </div>
+    </div>
+    <div className="card-body mt-4">
+        <p className="text-justify fs-5">
+            Após a cerimônia, teremos o prazer de recebê-los para a recepção no Salão Itamaraty da Steak Bull. A festa começará logo após o término da celebração na catedral. Queremos ressaltar a importância de estarem conosco desde a cerimônia até a recepção, para que possamos compartilhar juntos todos os momentos deste dia inesquecível.
+        </p>
+    </div>
+</div>
 
-                        <a className="nav-link" target="_blank" href="https://maps.app.goo.gl/ww61LAaK9ZgrtEeW8">
-                            <div className="text-center">
-                                <img src="./imagens/recepcao.svg" width="352px" height="205px" className="me-12" alt="Recepcao" />
-                            </div>
-                        </a>
+<div className="mt-4">
+    <div className="container">
+        <div className="row justify-content-center">
+            <div className="col-12 col-md-8 col-lg-6 text-center">
+                <a href="https://forms.gle/52Uy58jHBpuMQHW97" target="_blank" className="d-inline-block link-clicavel">
+                    <img 
+                        src="./imagens/presenca.svg" 
+                        className="img-fluid rounded-3 shadow-lg hover-scale" 
+                        alt="Confirmar Presença"
+                    />
+                </a>
+            </div>
+        </div>
+    </div>
+    <div className="card-body mt-4">
+        <p className="text-justify fs-5">
+            Para que possamos nos preparar da melhor forma para a nossa celebração, pedimos a gentileza de confirmar sua presença até o dia 20 de dezembro de 2024. A presença de cada um de vocês é muito importante para nós, mas entendemos que imprevistos podem ocorrer e outros compromissos podem já estar agendados para essa data.
+        </p>
+    </div>
+</div>
 
-                    </div>
-                    <div className="card-body">
-                        <p>Após a cerimônia, teremos o prazer de recebê-los para a recepção no Salão Itamaraty da Steak Bull. A festa começará logo após o término da celebração na catedral. Queremos ressaltar a importância de estarem conosco desde a cerimônia até a recepção, para que possamos compartilhar juntos todos os momentos deste dia inesquecível.</p>
-                    </div>
-                </div>
+<div className="mt-4">
+    <div className="container">
+        <div className="row justify-content-center">
+            <div className="col-12 col-md-8 col-lg-6 text-center">
+                <a href="/wedding-app/presentes" className="d-inline-block link-clicavel">
+                    <img 
+                        src="./imagens/presentes.svg" 
+                        className="img-fluid rounded-3 shadow-lg hover-scale" 
+                        alt="Presentes"
+                    />
+                </a>
+            </div>
+        </div>
+    </div>
+    <div className="card-body mt-4">
+        <p className="text-justify fs-5">
+            Para aqueles que desejarem nos presentear, ficaremos imensamente gratos. O que mais desejamos é a sua presença, mas, caso queira nos agraciar com um presente, preparamos uma lista especial com experiências que enriquecerão nossa vida juntos e criarão memórias afetivas inesquecíveis.
+        </p>
+    </div>
+</div>
 
-                <div className="mt-4">
-                    <div className="container">
-                        <a className="nav-link" target="_blank" href="https://forms.gle/52Uy58jHBpuMQHW97">
-                            <div className="text-center" style={{ marginTop: "36px" }}>
-                                <img src="./imagens/presenca.svg" width="355px" height="190px" className="me-12" alt="Presenca" />
-                            </div>
-                        </a>
-                    </div>
-                    <div className="card-body">
-                        <p>Para que possamos nos preparar da melhor forma para a nossa celebração, pedimos a gentileza de confirmar sua presença até o dia 20 de dezembro de 2024. A presença de cada um de vocês é muito importante para nós, mas entendemos que imprevistos podem ocorrer e outros compromissos podem já estar agendados para essa data.</p>
-                    </div>
-                </div>
+<div className="mt-4">
+    <div className="container">
+        <div className="row justify-content-center">
+            <div className="col-12 col-md-8 col-lg-6 text-center">
+                <a href="/wedding-app/padrinhos" className="d-inline-block link-clicavel">
+                    <img 
+                        src="./imagens/padrinhos.svg" 
+                        className="img-fluid rounded-3 shadow-lg hover-scale" 
+                        alt="Padrinhos"
+                    />
+                </a>
+            </div>
+        </div>
+    </div>
+    <div className="card-body mt-4">
+        <p className="text-justify fs-5">
+            Queridos padrinhos e madrinhas, é com imensa alegria que contamos com a presença e o apoio de vocês neste momento tão especial de nossas vidas. Sabemos que o papel de vocês vai muito além de estar ao nosso lado no altar. Vocês são parte essencial desta jornada, amigos queridos que escolhemos para compartilhar e celebrar nosso amor.
+        </p>
+    </div>
+</div>
 
-                <div className="mt-4">
-                    <div className="container">
-                        <a href="/wedding-app/presentes">
-
-                            <div className="text-center">
-                                <img src="./imagens/presentes.svg" width="355px" height="190px" className="me-12" alt="Presentes" />
-                            </div>
-                        </a>
-                    </div>
-                    <div className="card-body">
-                        <p>Para aqueles que desejarem nos presentear, ficaremos imensamente gratos. O que mais desejamos é a sua presença, mas, caso queira nos agraciar com um presente, preparamos uma lista especial com experiências que enriquecerão nossa vida juntos e criarão memórias afetivas inesquecíveis.</p>
-                    </div>
-                </div>
-
-                <div className="mt-4">
-                    <div className="container">
-                        <a href="/wedding-app/padrinhos">
-
-                            <div className="text-center">
-                                <img src="./imagens/padrinhos.svg" width="355px" height="190px" className="me-12" alt="Presentes" />
-                            </div>
-                        </a>
-                    </div>
-                    <div className="card-body">
-                        <p>Queridos padrinhos e madrinhas, é com imensa alegria que contamos com a presença e o apoio de vocês neste momento tão especial de nossas vidas. Sabemos que o papel de vocês vai muito além de estar ao nosso lado no altar. Vocês são parte essencial desta jornada, amigos queridos que escolhemos para compartilhar e celebrar nosso amor.</p>
-                    </div>
-                </div>
-
-                <div className="mt-4">
-                    <div className="container">
-                        <a href="/wedding-app/capsula">
-
-                            <div className="text-center">
-                                <img src="./imagens/capsula.svg" width="355px" height="190px" className="me-12" alt="Presentes" />
-                            </div>
-                        </a>
-                    </div>
-                    <div className="card-body">
-                        <p>Cápsula do Tempo do Casamento! Compartilhe suas Memórias e Ajude-nos a Guardar este Momento Especial.</p>
-                    </div>
-                </div>
+<div className="mt-4">
+    <div className="container">
+        <div className="row justify-content-center">
+            <div className="col-12 col-md-8 col-lg-6 text-center">
+                <a href="/wedding-app/capsula" className="d-inline-block link-clicavel">
+                    <img 
+                        src="./imagens/capsula.svg" 
+                        className="img-fluid rounded-3 shadow-lg hover-scale" 
+                        alt="Cápsula do Tempo"
+                    />
+                </a>
+            </div>
+        </div>
+    </div>
+    <div className="card-body mt-4">
+        <p className="text-justify fs-5">
+            Cápsula do Tempo do Casamento! Compartilhe suas memórias e ajude-nos a guardar este momento especial.
+        </p>
+    </div>
+</div>
 
 
                 <GuestMessages />
